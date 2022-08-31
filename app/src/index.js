132502760c34d34a61ad2932662e3c1cda62b8fb
Srijan Duggal <srijanduggal17@gmail.com>
2022-08-31 18:40:33 -0400

    adding cursor change and color change to icons

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,8 @@
 /* Set up blockly workspace */
 // var blocklyArea = document.getElementById('blocklyArea');
+const toggleOpenDiv = document.getElementById("toggleOpen");
+
+
 var blocklyDiv = document.getElementById('blocklyDiv');
 var workspace = Blockly.inject(blocklyDiv,
     {
@@ -23,8 +26,12 @@ var onresize = function (e) {
     var y = 0;
     blocklyDiv.style.left = x + 'px';
     blocklyDiv.style.top = y + 'px';
-    blocklyDiv.style.width = (window.innerWidth / 3 * 2) + 'px';
-    blocklyDiv.style.height = window.innerHeight + 'px';
+    if (window.getComputedStyle(toggleOpenDiv).getPropertyValue("display") == "none") {
+        blocklyDiv.style.width = (window.innerWidth / 3 * 2) + 'px';
+    } else {
+        blocklyDiv.style.width = window.innerWidth + 'px';
+    }
+    blocklyDiv.style.height = (window.innerHeight * 0.9)  + 'px';
     Blockly.svgResize(workspace);
 };
 window.addEventListener('resize', onresize, false);
@@ -83,21 +90,59 @@ uploadButton.addEventListener("click", () => {
     window.api.send("upload-code", fileContent);
 });
 
-// let iconsArr = ["newfilebtn", "openfilebtn", "savebtn", "saveasbtn", "uploadbtn"];
+const pythonArea = document.getElementById("textArea");
+const blocklyArea = document.getElementById("blocklyArea");
 
-// iconsArr.forEach(x => {
-//     let el = document.getElementById(x);
-//     el.onmouseover = function() {
-//         el.classList.toggle('fa-2x');
-//         el.classList.toggle('fa-3x');
-//     }
+const toggleClosedDiv = document.getElementById("toggleClosed");
 
-//     el.onmouseout = function() {
-//         el.classList.toggle('fa-2x');
-//         el.classList.toggle('fa-3x');
-//     }
-// });
+toggleClosedDiv.addEventListener("click", () => {
+    toggleClosedDiv.style.display = "none";
+    toggleOpenDiv.style.display = "block";
+    pythonArea.style.display = "none";
+    blocklyArea.classList.add("col-12");
+    blocklyArea.classList.remove("col-8");
+    blocklyDiv.style.width = window.innerWidth + 'px';
+    Blockly.svgResize(workspace);
+});
 
+toggleOpenDiv.addEventListener("click", () => {
+    toggleClosedDiv.style.display = "block";
+    toggleOpenDiv.style.display = "none";
+    pythonArea.style.display = "block";
+    blocklyArea.classList.remove("col-12");
+    blocklyArea.classList.add("col-8");
+    blocklyDiv.style.width = (window.innerWidth / 3 * 2) + 'px';
+    Blockly.svgResize(workspace);
+});
+
+
+let iconsArr = ["newfilebtn", "openfilebtn", "savebtn", "saveasbtn", "uploadbtn"];
+
+iconsArr.forEach(x => {
+    let el = document.getElementById(x);
+    el.onmouseover = function() {
+        el.style.color = '#0066B2';
+        el.style.cursor = 'pointer';
+    }
+
+    el.onmouseout = function() {
+        el.style.color = '';
+    }
+});
+
+let toggleIconsArr = ["toggleClosed", "toggleOpen"];
+toggleIconsArr.forEach(x => {
+    let el = document.getElementById(x);
+
+    el.onmouseover = () => {
+        el.style.cursor = 'pointer';
+        el.style.color = '#EC1C24';
+    }
+
+    el.onmouseout = () => {
+        el.style.color = '';
+    }
+})
 
 /* Code Cleaning and Formatting */
 function processCode(code) {
