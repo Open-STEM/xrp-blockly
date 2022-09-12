9d734118fe96d4f921ce189faaeba3f6a81fd12c
Srijan Duggal <srijanduggal17@gmail.com>
2022-09-12 19:10:01 -0400

    setting up react

--- public/electron.js
+++ public/electron.js
@@ -1,18 +1,12 @@
-// main.js
-
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog, protocol } = require('electron')
 const path = require('path');
 const fs = require('fs');
 const drivelist = require('drivelist');
 var AsyncPolling = require('async-polling');
 
-// try {
-//   require('electron-reloader')(module);
-// } catch { }
-
 let mainWindow;
-createWindow = () => {
+function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
@@ -31,12 +25,38 @@ createWindow = () => {
     mainWindow.focus();
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // In production, set the initial browser path to the local bundle generated
+  // by the Create React App build process.
+  // In development, set it to localhost to allow live/hot-reloading.
+  const appURL = app.isPackaged
+    ? url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    : "http://localhost:3000";
+  mainWindow.loadURL(appURL);
   mainWindow.setMenu(null);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // Automatically open Chrome's DevTools in development mode.
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+// Setup a local proxy to adjust the paths of requested files when loading
+// them from the local production bundle (e.g.: local fonts, etc...).
+function setupLocalFilesNormalizerProxy() {
+  protocol.registerHttpProtocol(
+    "file",
+    (request, callback) => {
+      const url = request.url.substr(8);
+      callback({ path: path.normalize(`${__dirname}/${url}`) });
+    },
+    (error) => {
+      if (error) console.error("Failed to register protocol");
+    }
+  );
 }
 
 // This method will be called when Electron has finished
@@ -44,11 +64,12 @@ createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   ipcMain.handle('load-appstate', () => {
-    return JSON.parse(fs.readFileSync("./app/state.json"));
+    return JSON.parse(fs.readFileSync("./state.json"));
   });
   createWindow()
+  setupLocalFilesNormalizerProxy();
 
-  app.on('activate', () => {
+  app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
@@ -71,17 +92,18 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
+app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
+
 /** APIs **/
 // Use ipc to receive text to save to a file using system save dialog
 ipcMain.on('save-code', (event, req) => {
-  const appState = JSON.parse(fs.readFileSync("./app/state.json"));
+  const appState = JSON.parse(fs.readFileSync("./state.json"));
   if (appState.fullPath != "") {
     let filePath = path.join(appState.fullPath, req.filename);
     fs.writeFileSync(filePath, req.content);
@@ -108,7 +130,7 @@ function updateStateJson(appState, slice) {
   for (const [key, value] of Object.entries(slice)) {
     appState[key] = value;
   }
-  fs.writeFileSync("./app/state.json", JSON.stringify(appState));
+  fs.writeFileSync("./state.json", JSON.stringify(appState));
   // TODO: update state in frontend?
 }
 
