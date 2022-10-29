ec2062be37536c25f4c70957c04a068fc7a86ac3
Yoni <yweiner@wpi.edu>
2022-10-29 19:35:02 -0400

    color

--- app/src/xrp_blocks_python.js
+++ app/src/xrp_blocks_python.js
@@ -128,6 +128,12 @@ Blockly.Python['xrp_led_br'] = function (block) {
   return code;
 };
 
+Blockly.Python['xrp_led_co'] = function (block) {
+  var value_color_hex = Blockly.Python.valueToCode(block, 'color', Blockly.Python.ORDER_ATOMIC);
+  var code = `led.set_color(${value_color_hex})`;
+  return code;
+};
+
 Blockly.Python['xrp_servo_deg'] = function (block) {
   var value_degrees = Blockly.Python.valueToCode(block, 'degrees', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
